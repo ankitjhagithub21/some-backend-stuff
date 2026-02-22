3d63fe12cfdc8e6cf58175dc4953b5ffedf30993
ankit <ajha45075@gmail.com>
2026-02-22 16:27:39 +0530

    rate-limiter

---
+++ rate-limiter/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const rateLimit = require("express-rate-limit");
+const app = express();
+
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000,
+    limit:2,
+	message: async (req, res) => {
+		res.json({message:"You have exceeded the limit of 2 requests per minute"})
+	},
+})
+
+
+app.get("/", limiter, (req, res) => {
+    res.json({message:"Hello from the server."})
+});
+
+app.listen(8000, () => {
+    console.log("Server started on port 8000");
+});
+
